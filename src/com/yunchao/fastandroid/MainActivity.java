de07fae1d6de1c9f60e82718b3e193d926cfa9bb
ifjgm005 <ifjgm005@163.com>
2015-12-12 00:44:21 +0800

    增加主界面

---
+++ src/com/yunchao/fastandroid/MainActivity.java
@@ -0,0 +1,10 @@
+package com.yunchao.fastandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
